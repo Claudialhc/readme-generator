9a701d659abec69e4eca8156dea0064826f9ddbc
Claudia Hernandez <claudiahdez13@gmail.com>
2021-01-07 22:18:30 -0800

    installed inquirer package and imported it to the code

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// ## have done before
+// - use fs module to create readme file
+// - use template literals to inesrt user input into readme string
+// - reference good readme for a template
+// ## need to learn
+// - use inquirer package to get user input (thursday)
+// - create a .gitignore file (on your own/office hours)
+// - create package.json file (thursday)
+// - how to make a video that captures screen and voice (on your own/pre-work)
+// - learn what a badge is and how to render one in markdown (on your own/office hours)
